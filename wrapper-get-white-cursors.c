48323b8ad45db5f9f886984e063e88f4a97d7ea9
spartrekus <spartrekus@gmail.com>
2017-08-26 01:23:14 +0200

    Add files via upload

---
+++ wrapper-get-white-cursors.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+int main( int argc, char *argv[])
+{
+    ////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////
+    if ( argc == 3)
+      if ( strcmp( argv[1] , "get" ) ==  0 ) 
+      if ( ( strcmp( argv[2] , "cursor" ) ==  0 ) 
+      || ( strcmp( argv[2] , "cursors" ) ==  0 ) )
+      {
+          printf( " GET CURSOR \n" );
+          system( " curl https://codeload.github.com/spartrekus/x11-white-cursor/zip/master > file.zip  " );
+          if ( fexist( "file.zip" ) == 1 ) 
+	    system( " unzip file.zip  ; unzip x11-white-cursor-master/x11-white-cursor-3.zip  " );
+          printf( " GET CURSOR - DONE \n" );
+	  return 0;
+      }
+   return 0;
+}
+
+
+
