d1d825aa7719e110ee003ea8f8a5eb76b4db2600
spartrekus <spartrekus@gmail.com>
2017-08-30 09:57:34 +0200

    Update wrapper-get-white-cursors.c

--- wrapper-get-white-cursors.c
+++ wrapper-get-white-cursors.c
@@ -23,6 +23,26 @@ int main( int argc, char *argv[])
           printf( " GET CURSOR - DONE \n" );
 	  return 0;
       }
+	
+	
+	    ////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////
+    if ( argc == 3)
+      if ( strcmp( argv[1] , "get" ) ==  0 ) 
+      if ( ( strcmp( argv[2] , "crystalwhite" ) ==  0 ) 
+      || ( strcmp( argv[2] , "crystal" ) ==  0 ) )
+      {
+          printf( " GET CURSOR \n" );
+          system( " curl https://raw.githubusercontent.com/spartrekus/x11-white-cursor/master/crystalwhite-cursor.zip > file.zip  " );
+          if ( fexist( "file.zip" ) == 1 ) 
+	    system( " unzip file.zip    " );
+          printf( " GET CURSOR - DONE \n" );
+	  return 0;
+      }
+	
+	
+	
+	
    return 0;
 }
 
